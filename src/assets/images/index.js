312d2b195876021f99011892bc43060403ae37f6
IamArshadAli <arshadalikaldane@gmail.com>
2023-12-06 00:19:16 +0530

    assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import iconBookmark from "./icon-bookmark.svg";
+import iconCheck from "./icon-check.svg";
+import iconCloseMenu from "./icon-close-menu.svg";
+import iconCloseModal from "./icon-close-modal.svg";
+import iconHamburger from "./icon-hamburger.svg";
+import imageHeroDesktop from "./image-hero-desktop.jpg";
+import imageHeroMobile from "./image-hero-mobile.jpg";
+import logoMastercraft from "./logo-mastercraft.svg";
+import logo from "./logo.svg";
+
+export {
+  iconBookmark,
+  iconCheck,
+  iconCloseMenu,
+  iconCloseModal,
+  iconHamburger,
+  imageHeroDesktop,
+  imageHeroMobile,
+  logoMastercraft,
+  logo,
+};
